f17291ea957c92f28a5c731d0d2f4765932ac6fc
kapil <kapss787@gmail.com>
2023-04-13 23:03:17 +0530

    second commit

--- First.java
+++ First.java
@@ -2,5 +2,6 @@ class First{
     public static void main(String[] args) {
         System.out.println("first hello");
         System.out.println("second");
+        System.out.println("third");
     }
 }
