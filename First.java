2be102ca0a1cdf8dd35b432debfee765b5569d63
kapil <kapss787@gmail.com>
2023-04-13 23:24:35 +0530

    three file com

---
+++ First.java
@@ -0,0 +1,8 @@
+class First{
+    public static void main(String[] args) {
+        System.out.println("first hello");
+        System.out.println("second");
+        System.out.println("third");
+        System.out.println("hi");
+    }
+}
--- Two.java
+++ Two.java
@@ -1,6 +1,7 @@
-public class Two {
+class Two {
     public static void main(String[] args) {
         System.out.println("four ");
+        System.out.println("new changeclea");
         System.out.println("hi iam from f1 feature ");
     }
 }
